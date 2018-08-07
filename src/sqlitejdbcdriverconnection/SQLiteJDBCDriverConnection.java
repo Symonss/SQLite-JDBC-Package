df7ff4e8e514c70bf4f4c1beeaa4408cad4ea2b1
symonss <somborisymonss@gmail.com>
2018-08-07 22:53:16 +0300

    SQLITE-JDBC CONNECTIONS

--- src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
+++ src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
@@ -2,6 +2,7 @@
 package sqlitejdbcdriverconnection;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -9,7 +10,7 @@ import java.sql.SQLException;
  *
  * @author Symons
  */
-public class SQLiteJDBCDriverConnection {
+public class SQLiteJDBCDriverConnection {    
   /**
      * Connect to a sample database
      */
@@ -35,11 +36,36 @@ public class SQLiteJDBCDriverConnection {
             }
         }
     }
+    
+    /*
+    Creating a database!
+     1.First, specify the database name and its location e.g., c:\sqlite\db\test.db
+     2.Second, connect to the database via SQLite JDBC driver.
+    3.By simply connecting to a database that do not exitst SQLite creates the database
+    */
+     public static void createNewDatabase(String fileName) {
+ 
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+ 
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    
     /**
-     * @param args the command line arguments
+     * @Main method
      */
     public static void main(String[] args) {
-        connect();
+       // connect();
+       createNewDatabase("test.db");
     }
     
 }
